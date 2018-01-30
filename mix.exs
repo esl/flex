fb2d91b3554fc2a0e9f3bde68d7291e29c0adf97
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2018-01-30 12:47:03 +0100

    Align columns in Mix dependency list into a table-like format

--- mix.exs
+++ mix.exs
@@ -22,11 +22,11 @@ defmodule Influx.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.13.0"},
-      {:poison, "~> 2.2"},
-      {:ex_doc, "~> 0.18.1", only: :dev},
-      {:earmark, "~> 1.2.2", only: :dev},
-      {:dialyxir, "~> 0.5.1", only: :dev},
-      {:credo, "~> 0.8.8", only: :dev},
+      {:poison,    "~> 2.2"},
+      {:ex_doc,    "~> 0.18.1", only: :dev},
+      {:earmark,   "~> 1.2.2",  only: :dev},
+      {:dialyxir,  "~> 0.5.1",  only: :dev},
+      {:credo,     "~> 0.8.8",  only: :dev},
     ]
   end
 end
