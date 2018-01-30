c4f87daa6972b67dfd022104aa8066dee7384c2a
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2018-01-30 12:49:52 +0100

    Relax dependencies to their minor versions + update Mix lock file

--- mix.exs
+++ mix.exs
@@ -21,12 +21,12 @@ defmodule Influx.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 0.13.0"},
+      {:httpoison, "~> 0.13"},
       {:poison,    "~> 2.2"},
-      {:ex_doc,    "~> 0.18.1", only: :dev},
-      {:earmark,   "~> 1.2.2",  only: :dev},
-      {:dialyxir,  "~> 0.5.1",  only: :dev},
-      {:credo,     "~> 0.8.8",  only: :dev},
+      {:ex_doc,    "~> 0.18", only: :dev},
+      {:earmark,   "~> 1.2",  only: :dev},
+      {:dialyxir,  "~> 0.5",  only: :dev},
+      {:credo,     "~> 0.8",  only: :dev},
     ]
   end
 end
