c97425591fac71cbafa86e2b014c269a39836415
Bartosz Szafran <bartosz.szafran@erlang-solutions.com>
2018-08-16 11:10:39 +0200

    Bump elixir version to 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Flex.Mixfile do
     [
       app: :flex,
       version: "0.2.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.6",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps()
